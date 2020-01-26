1fdcdc981c8dbea5792ff3b5a2f81b4e685f2cd5
Valerii Kovalchuk <kovvalole@gmail.com>
2020-01-26 15:01:29 +0200

    Change color theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 import { Layout } from 'components/layout';
 import { SignIn } from 'components/authentication';
 import { AccountListPage, AccountPage } from 'components/accounts';
 import urls from 'helpers/urls';
+import theme from 'helpers/theme';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route path={urls.login}>
-            <SignIn />
-          </Route>
-          <Route path={urls.account}>
-            <AccountPage />
-          </Route>
-          <Route path={urls.home}>
-            <AccountListPage />
-          </Route>
-        </Switch>
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Switch>
+            <Route path={urls.login}>
+              <SignIn />
+            </Route>
+            <Route path={urls.account}>
+              <AccountPage />
+            </Route>
+            <Route path={urls.home}>
+              <AccountListPage />
+            </Route>
+          </Switch>
+        </Layout>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
---
+++ src/helpers/theme.ts
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import lightGreen from '@material-ui/core/colors/lightGreen';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: lightGreen,
+  },
+});
+
+export default theme;
